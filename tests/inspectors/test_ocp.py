c39698cde46b6230eb6b892fd59960b74e09b154
PRASHANTH RAO N <68043931+PRASHANTH337@users.noreply.github.com>
2020-10-06 13:27:29 +0530

    Update test_ocp.py

--- tests/inspectors/test_ocp.py
+++ tests/inspectors/test_ocp.py
@@ -154,6 +154,57 @@ class TestOCP:
     def test_pod_ready(self, obj, expected):
         result = OCP.pod_ready(obj)
         assert result == expected
+        
+    @pytest.mark.parametrize(
+        "obj,expected",
+        [
+            (
+                dict(
+                    metadata=dict(name='test_pod'),
+                    status=dict(containerStatuses=[
+                        dict(
+                            name='test_container',
+                            ready=False,
+                            state=dict(
+                                terminated=dict(
+                                    reason='garbage',
+                                ),
+                            ),
+                        ),
+                    ]),
+                ),
+                ["Container 'test_container' in pod 'test_pod' is not ready"],
+            ),
+            (
+                dict(
+                    metadata=dict(name='test_pod'),
+                    status=dict(containerStatuses=[
+                        dict(
+                            name='test_container',
+                            ready=False,
+                            state=dict(
+                                terminated=dict(
+                                    reason='completed',
+                                    exitCode=0,
+                                ),
+                            ),
+                        ),
+                    ]),
+                ),
+                list(),
+            ),
+            (
+                dict(
+                    metadata=dict(name='test_pod'),
+                    status=dict(),
+                ),
+                [dict(
+                    pod_name='test_pod',
+                    status=dict(),
+                )],
+            ),
+        ]
+    )    
     def test_restart_count(self, obj, expected):
         result = OCP.restart_count(obj)
         assert result == expected
